65bb28833d711cee09aeb905ec531bf7eaf15381
hasaannn327-source <hasaannn327@gmail.com>
2025-07-31 20:34:00 +0300

    Update drawPlanSVG.js

--- backend/helpers/drawPlanSVG.js
+++ backend/helpers/drawPlanSVG.js
@@ -1,1 +1,18 @@
+const planData = require("./planData.json");
 
+function generatePlan(daireTipi, cepheSayisi, metrekare) {
+  const plan = planData[daireTipi]?.[cepheSayisi];
+  if (!plan) throw new Error("Uygun plan verisi bulunamadı");
+
+  const { genislik, yukseklik, svgRenk, odalar } = plan;
+
+  let svg = `<svg width="${genislik}" height="${yukseklik}" xmlns="http://www.w3.org/2000/svg">`;
+  svg += `<rect x="0" y="0" width="${genislik}" height="${yukseklik}" fill="${svgRenk}" stroke="black" />`;
+
+  odalar.forEach((oda, i) => {
+    svg += `<text x="10" y="${20 + i * 20}" font-size="14">${oda}</text>`;
+  });
+
+  svg += `</svg>`;
+  return svg;
+}
